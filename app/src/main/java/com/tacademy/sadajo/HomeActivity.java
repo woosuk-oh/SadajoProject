b4b766ef675188a71adfeacbad59ef06fcbb573d
eunzy030 <eunzy.p@gmail.com>
2016-11-15 18:06:11 +0900

    add navigation icon and modify activity_home

--- app/src/main/java/com/tacademy/sadajo/HomeActivity.java
+++ app/src/main/java/com/tacademy/sadajo/HomeActivity.java
@@ -48,58 +48,6 @@ public class HomeActivity extends AppCompatActivity {
         mypageBtn.setOnClickListener(mClickListener);
 
 
-
-
-//       Button button  = (Button)findViewById(R.id.testBtn) ;
-
-//        tabLayout.addTab(tabLayout.newTab().setText("홈").setIcon(R.drawable.heart_black));
-//        tabLayout.addTab(tabLayout.newTab().setText("검색").setIcon(R.drawable.heart_black));
-//        tabLayout.addTab(tabLayout.newTab().setText("쇼핑리스트").setIcon(R.drawable.heart_black));
-//        tabLayout.addTab(tabLayout.newTab().setText("채팅").setIcon(R.drawable.heart_black));
-//        tabLayout.addTab(tabLayout.newTab().setText("마이페이지").setIcon(R.drawable.heart_black));
-//
-
-//
-//        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-//            @Override
-//            public void onTabSelected(TabLayout.Tab tab) {
-//              switch (tab.getPosition()){
-//                  case 0 :
-//                      tab.setIcon(R.drawable.selector_home);
-//                     // Intent intent = new Intent(HomeActivity.this, SearchActivity.class);
-//                      break;
-//                  case 1 :
-//                      tab.setIcon(R.drawable.selector_home);
-//                      Intent intent = new Intent(HomeActivity.this, SearchActivity.class);
-//                      startActivity(intent);
-//                      //overridePendingTransition(0,0);
-//                      break;
-//                  case 2 :
-//                      tab.setIcon(R.drawable.selector_home);
-//                      break;
-//                  case 3 :
-//                      tab.setIcon(R.drawable.selector_home);
-//                      break;
-//                  case 4 :
-//                      tab.setIcon(R.drawable.selector_home);
-//                      break;
-//
-//
-//              }
-//            }
-//
-//            @Override
-//            public void onTabUnselected(TabLayout.Tab tab) {
-//
-//            }
-//
-//            @Override
-//            public void onTabReselected(TabLayout.Tab tab) {
-//
-//            }
-//        });
-
-
     }
 
     ImageButton.OnClickListener  mClickListener = new View.OnClickListener() {
@@ -124,7 +72,7 @@ public class HomeActivity extends AppCompatActivity {
                     startActivity(intent);
                     break;
                 case R.id. chattingBtn:
-                    homeBtn.setSelected(false); //   homeBtn.setImageResource(R.drawable.heart_black);
+                    homeBtn.setSelected(false);
                     intent =  new Intent(HomeActivity.this, ChattingActivity.class);
                     startActivity(intent);
                     break;
