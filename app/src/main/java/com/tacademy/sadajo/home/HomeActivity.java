c393a8c7d5a63b369b111faa74bc6a015255b2bb
eunzy030 <eunzy.p@gmail.com>
2016-11-22 18:16:38 +0900

    modified : home_linearlayout3

--- app/src/main/java/com/tacademy/sadajo/home/HomeActivity.java
+++ app/src/main/java/com/tacademy/sadajo/home/HomeActivity.java
@@ -1,11 +1,15 @@
 package com.tacademy.sadajo.home;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -18,6 +22,10 @@ import com.tacademy.sadajo.BottomBarClickListener;
 import com.tacademy.sadajo.R;
 import com.tacademy.sadajo.fonts.NanumRegularTextView;
 import com.tacademy.sadajo.mypage.MyPageActivity;
+import com.tacademy.sadajo.shoppinglist.ShoppingListData;
+import com.tacademy.sadajo.shoppinglist.ShoppingListSample;
+
+import java.util.ArrayList;
 
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
@@ -31,7 +39,7 @@ public class HomeActivity extends AppCompatActivity {
     ImageButton chattingBtn;
     ImageButton mypageBtn;
 
-    ImageView profileImageView;
+   // ImageView profileImageView;
     TextView countryNameTv;
     TextView scheduleTv;
     TextView dateComeTv;
@@ -42,6 +50,8 @@ public class HomeActivity extends AppCompatActivity {
     TextView card2CountryTv;
     Button scheduleBtn;
 
+
+    HomeUserRecyclerViewAdapter homeUserRecyclerViewAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -83,10 +93,11 @@ public class HomeActivity extends AppCompatActivity {
         countryNameTv = (TextView)findViewById(R.id.countryNameTv);
         scheduleTv = (TextView)findViewById(R.id.scheduleTv);
 
-        profileImageView = (ImageView)findViewById(R.id.profileImageView);
+        //profileImageView = (ImageView)findViewById(R.id.profileImageView);
+        //  profileImageView.setOnClickListener(onClickListener);
 
         scheduleBtn.setOnClickListener(onClickListener);
-        profileImageView.setOnClickListener(onClickListener);
+
 
 
         LinearLayout linearLayout =(LinearLayout)findViewById(R.id.cardView2LL); //두번째 카드뷰 리니어레이아웃
@@ -109,9 +120,101 @@ public class HomeActivity extends AppCompatActivity {
         linearLayout2.addView(button);
 
 
+        //layout3
+      //  CustomRecyclerDecoration decoration = new CustomRecyclerDecoration(30);
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(HomeActivity.this,4));
+     //  recyclerView.addItemDecoration(decoration);
+         homeUserRecyclerViewAdapter = new HomeUserRecyclerViewAdapter(HomeActivity.this, ShoppingListSample.shoppinList);
+        recyclerView.setAdapter(homeUserRecyclerViewAdapter);
 
 
 
+    }
+
+    public static class HomeUserRecyclerViewAdapter
+            extends RecyclerView.Adapter<HomeUserRecyclerViewAdapter.ViewHolder> {
+
+        private ArrayList<ShoppingListData> shoppingListDatas;
+        private Context context;
+
+        public HomeUserRecyclerViewAdapter(Context context, ArrayList<ShoppingListData> shoppingListDatas) {
+            this.context = context;
+            this.shoppingListDatas = shoppingListDatas;
+        }
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+
+            public final View mView;
+
+            public final TextView homeUserIdTextView;
+            public final ImageView userProfileImageView;
+
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+
+                homeUserIdTextView = (TextView) view.findViewById(R.id.homeUserIdTextView);
+                userProfileImageView = (ImageView) view.findViewById(R.id.userProfileImageView);
+
+            }
+        }
+
+        @Override
+        public HomeUserRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(
+                    R.layout.home_layout3_recyclerview_item, parent, false);
+
+            return new HomeUserRecyclerViewAdapter.ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final HomeUserRecyclerViewAdapter.ViewHolder holder, final int position) {
+
+
+//            holder.shoppinglistCountryNameTextView.setText(shoppingListDatas.get(position).getCountryName().toString());
+//            holder.shoppinglistDateTextView.setText(shoppingListDatas.get(position).getTravelDate().toString());
+//            holder.shoppinglistFolderImageView.setImageResource(R.drawable.mark);
+
+            holder.homeUserIdTextView.setText("닉네임");
+
+            holder.userProfileImageView.setImageResource(R.drawable.profile_empty);
+
+
+
+//            Glide.with(GirlsApplication.getGirlsContext())
+//                    .load(girlInfo)
+//                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+//                    .animate(android.R.anim.slide_in_left)
+//                    .into(holder.girlsImage);
+
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+//                    Intent intent = new Intent(GirlsApplication.getGirlsContext(), GirlsMemberDetailActivity.class);
+//                    intent.putExtra("memberImage", girlsImages.get(position));
+//                    intent.putExtra("memberName", holder.memberName.getText().toString());
+//
+//                    ActivityOptionsCompat options =
+//                            ActivityOptionsCompat.makeSceneTransitionAnimation(
+//                                    owner, holder.girlsImage, ViewCompat.getTransitionName(holder.girlsImage));
+//
+//                    ActivityCompat.startActivity(owner, intent, options.toBundle());
+                    Intent intent = new Intent(context,MyPageActivity.class);
+                    context.startActivity(intent);
+
+                }
+            });
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return shoppingListDatas.size();
+        }
     }
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
@@ -125,10 +228,10 @@ public class HomeActivity extends AppCompatActivity {
                     dialog.show();
                     break;
 
-                case R.id.profileImageView :
-                    Intent intent = new Intent(HomeActivity.this, MyPageActivity.class);
-                    startActivity(intent);
-                    break;
+//                case R.id.profileImageView :
+//                    Intent intent = new Intent(HomeActivity.this, MyPageActivity.class);
+//                    startActivity(intent);
+//                    break;
 
             }
         }
