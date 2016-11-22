695176c9355280f4a668e96c2df0e76b8d920e37
woosuk-oh <gta2v@naver.com>
2016-11-22 11:27:52 +0900

    SearchList Detail Update 11-22 11:22

--- app/src/main/java/com/tacademy/sadajo/search/searchlist/ItemArrayList.java
+++ app/src/main/java/com/tacademy/sadajo/search/searchlist/ItemArrayList.java
@@ -26,32 +26,5 @@ public class ItemArrayList {
         this.itemname= itemname;
     }
 
-/*
-    private static ArrayList<Integer> imageResources;
-    //  private static String GIRLS_IMAGE_URI= "http://192.168.0.196:5678/androidNetwork/girlsGeneration/";
-    static {
-        imageResources = new ArrayList<Integer>();
-        imageResources.add(R.drawable.sample1);
-        imageResources.add(R.drawable.sample2);
-        imageResources.add(R.drawable.sample3);
-        imageResources.add(R.drawable.sample4);
-        imageResources.add(R.drawable.sample5);
-    }
-    private static HashMap<Integer, String> nameMaps;
-
-    static {
-        nameMaps = new HashMap<Integer, String>();
-        nameMaps.put(R.drawable.sample1, "소녀시대멤버1");
-        nameMaps.put(R.drawable.sample2, "효연1");
-        nameMaps.put(R.drawable.sample3, "제시카1");
-        nameMaps.put(R.drawable.sample4, "서현1");
-        nameMaps.put(R.drawable.sample5, "써니1");
-    }
-
-
-
-    public static String  getGirlGroupName(int key) {
-        return nameMaps.get(key);
-    }*/
 }
 
