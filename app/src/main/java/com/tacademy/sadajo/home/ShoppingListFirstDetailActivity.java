61a92bc95873e2dd47f75895e1a38906bc7e588e
eunzy030 <eunzy.p@gmail.com>
2016-11-24 17:36:24 +0900

    Added :  ShoppingListFirstDetailActivity

---
+++ app/src/main/java/com/tacademy/sadajo/home/ShoppingListFirstDetailActivity.java
@@ -0,0 +1,15 @@
+package com.tacademy.sadajo.home;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tacademy.sadajo.R;
+
+public class ShoppingListFirstDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list_first_detail);
+    }
+}
