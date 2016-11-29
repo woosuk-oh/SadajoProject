456a0bfc6eefe7c8db473dc2896f218b6e1c4a3d
eunzy030 <eunzy.p@gmail.com>
2016-11-29 18:03:24 +0900

    added : hashmap

--- app/src/main/java/com/tacademy/sadajo/CountryCodeHashMap.java
+++ app/src/main/java/com/tacademy/sadajo/CountryCodeHashMap.java
@@ -6,10 +6,24 @@ import java.util.HashMap;
  * Created by EUNZY on 2016. 11. 29..
  */
 
-public class CountryCodeHashMap {
-    HashMap<String,String> countryCodeHash;
+public  class CountryCodeHashMap {
+   static HashMap<String,String> countryCodeHash = new HashMap<>();
     static {
-       // countryCodeHash = new
+        countryCodeHash.put("USA","미국");
+        countryCodeHash.put("JPN","일본");
+        countryCodeHash.put("HKG","홍콩");
+        countryCodeHash.put("ITA","이탈리아");
+        countryCodeHash.put("THA","태국");
+        countryCodeHash.put("AUS","호주");
+        countryCodeHash.put("CHN","중국");
+        countryCodeHash.put("PHL","필리핀");
+        countryCodeHash.put("PHL","대만");
 
+    }
+
+    public String getCountryCode(String name){
+
+
+        return countryCodeHash.get(name);
     }
 }
